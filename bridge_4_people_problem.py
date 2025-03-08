a6e5f7ce8abce0ee43990f4a78bb85d398a80854
Ankit Nakarmi <39302574+Nakarmi23@users.noreply.github.com>
2025-03-08 21:56:49 +0545

    feat: handle updating game state

--- bridge_4_people_problem.py
+++ bridge_4_people_problem.py
@@ -12,50 +12,56 @@ def renderGameState(leftState, rightState, flashLightState, currentCost):
     leftPeople  = ', '.join(leftState) if len(leftState) else "Empty"
     rightPeople  = ', '.join(rightState) if len(rightState) else "Empty"
 
-    print(f"{leftPeople} {'(Flashlight)' if flashLightState == 'L' else ''}  ------------- {'(Flashlight)' if flashLightState == 'R' else ''} {rightPeople}")
+    print(f"{leftPeople} {'(Flashlight)' if flashLightState == 'L' else ''} ------------- {'(Flashlight)' if flashLightState == 'R' else ''} {rightPeople}")
     print(f"\nCurrent Cost: {currentCost}")
 
-def parseSelectedOptions(individuals, input):
+def parseSelectedOptions(individuals, validOptions, input):
     parsed = [int(selected) for selected in input.split(",") if selected]
 
     if(len(parsed) < 1 or len(parsed) > 2):
         raise Exception("Must select a minimum of 1 option and a maximum of 2 option")
 
     if(not(all([(selected >= 1 and selected <=4) for selected in parsed]))):
-        raise Exception("Invalid range detected: valid rnage 1-4")
+        raise Exception("Invalid range detected: valid range 1-4")
     
-    return [individuals[selected - 1] for selected in parsed]
+    parsed = [individuals[selected - 1] for selected in parsed]
+    
+    for selected in parsed:
+        if(selected not in validOptions): raise Exception("Invalid selection: Valid options are: "+ ', '.join(validOptions))
+    
+    return parsed
 
 
 def main():
     state = {
-        "Elderly": "L",
-        "Adult": "L",
+        "Teen": "L",
         "Young Adult": "L",
-        "Teen": "L"
+        "Adult": "L",
+        "Elderly": "L",
     }
     individualCost = {
-        "Elderly": 10,
-        "Adult": 5,
+        "Teen": 1,
         "Young Adult": 2,
-        "Teen": 1
+        "Adult": 5,
+        "Elderly": 10,
         }
     totalCost = 0
     flashlight = "L"
     
 
     while 1:
-        if(all(value == "R" for value in state.values())):
-            print("\nYou've successfully moved everyone to the right.")
-            break
 
         left = [key for key, value in state.items() if value == "L"]
         right = [key for key, value in state.items() if value == "R"]
 
         renderGameState(left, right, flashlight, totalCost)
 
+        if(all(value == "R" for value in state.values())):
+            print("\nYou've successfully moved everyone to the right.")
+            break
+
         currentOptions = [
-            f"{index+1} = {value}" for index, value in enumerate(state.keys()) if (
+            f"{index+1} = {value}({individualCost[value]})" for index, value in enumerate(state.keys()) if (
                 value in (left if flashlight == "L" else right)
             )
         ]
@@ -63,16 +69,23 @@ def main():
         rawSelectedOptions = input(f"\nChoose one or two individuals to cross the bridge. You can only select a max of 2 people to cross the bridge at a time ({", ".join(currentOptions)}): ")
 
         try:  
-            parsedSelectedOptions = parseSelectedOptions(list(state.keys()), rawSelectedOptions)
+            parsedSelectedOptions = parseSelectedOptions(list(state.keys()), list(left if flashlight == 'L' else right), rawSelectedOptions)
         except Exception as e:
             print("Error: ", e)
             print("\n-------------------\n")
             continue
 
-
-        print(parsedSelectedOptions)
         
-        print("\n-------------------\n")
+        flashlight = 'R' if flashlight == 'L' else 'L'
+
+        totalCost = totalCost + max(individualCost[parsedSelectedOptions[0]], individualCost[parsedSelectedOptions[1]] if len(parsedSelectedOptions) == 2 else 0)
+
+        for selected in parsedSelectedOptions:
+            state[selected] = 'R' if state[selected] == 'L' else 'L'
+
+                         
+        
+        print("\n----------Game State Updated---------\n")
 
 
         
